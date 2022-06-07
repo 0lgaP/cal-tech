0a4541aaca74120b0ec52eb933157522b5420f63
0lgatron <perronolga@gmail.com>
2022-06-07 11:46:59 -0400

    edge cases

--- calculator.js
+++ calculator.js
@@ -1,43 +1,47 @@
-const expression = process.argv[2].trim()
-
 // Find innermost equasion
-const innerAdd = str => str.substring(str.lastIndexOf("(") + 1, str.indexOf(")"))
+const innerAdd = (str) =>
+  str.substring(str.lastIndexOf("(") + 1, str.indexOf(")"));
+
+// Changing regex function
+const innerSlice = (str) =>
+  str.slice(str.lastIndexOf("(") + 1, str.indexOf(")"));
 
 // Return the sum or product
-const calculate = str => {
-  
-  let exp = innerAdd(str).split(" ")
-  if (exp[0].toLowerCase() === "add"){
-    return +exp[1] + +exp[2]
+const calculate = (str) => {
+  let exp = innerAdd(str).toLowerCase().split(" ");
+  let calc = exp
+    .slice(1)
+    .map((element) => parseInt(element))
+    .filter(Boolean);
+
+  switch (exp[0]) {
+    case "add":
+      return calc.reduce((acc, cur) => acc + cur, 0);
+      break;
+    case "multiply":
+      return calc.reduce((acc, cur) => acc * cur, 1);
+      break;
+    default:
+      console.log("Please enter valid expression");
+      process.exit();
+      break;
   }
-  if (exp[0].toLowerCase() === "multiply"){
-    return +exp[1] * +exp[2]
-  }
-}
+};
 
-// Changing regex
-const innerSlice = str => str.slice(str.lastIndexOf("(") + 1, str.indexOf(")"))
-
-let curString = expression
-
-const resolve = str => {
-  let changingReg = `\(${innerSlice(str)}\)`
-  return curString.replace(changingReg, calculate(curString))
-}
-
-// Find All Brackets RegEx
-const brakets = /(?<=\().*(?=\))/g
+// Find Brackets RegEx
+const brakets = /(?<=\().*(?=\))/g;
 
 // Recursive function
-const calculator = str => {
-
-  if(str.match(brakets)){
-    curString = resolve(str)
-    return calculator(curString)
+const calculator = (str) => {
+  let curString;
+  if (str.match(brakets)) {
+    curString = str;
+    let changingReg = `\(${innerSlice(curString)}\)`;
+    curString = curString.replace(changingReg, calculate(curString));
+    return calculator(curString);
   } else {
-    return str
+    return str;
   }
-  
-}
+};
 
-console.log(calculator(curString))
+console.log(calculator(process.argv[2].trim()));
